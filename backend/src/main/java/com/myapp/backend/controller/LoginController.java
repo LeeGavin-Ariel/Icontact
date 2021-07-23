7ce351a63275fde5a074124a31423cdd74c81076
JWeonseok <weonseok789@gmail.com>
2021-07-23 16:42:44 +0900

    Update login service

--- backend/src/main/java/com/myapp/backend/controller/LoginController.java
+++ backend/src/main/java/com/myapp/backend/controller/LoginController.java
@@ -43,74 +43,24 @@ public class LoginController {
     public ResponseEntity<LoginResultDto> login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
         User user = loginRepository.findByUserIdAndPassword(loginDto.getUserId(), loginDto.getPassword());
         if(user != null){
-            LoginResultDto loginResultDto = new LoginResultDto();
             int userType = user.getType();
-            if(userType == 1){ // 학부모
 
-                Kid kid = user.getKids().get(0);
+            LoginResultDto loginResultDto = loginService.setLoginResult(userType, user);
 
-                if(kid.getAccept() == 2){
+            if (loginResultDto.getAccept() == 2){
+                TokenDto tokenDto = jwtService.create("userid", loginResultDto.getUserId(), "user");
 
-                    loginResultDto.setUserId(user.getUserId());
-                    loginResultDto.setUserName(user.getUserName());
-                    loginResultDto.setType(user.getType());
-                    loginResultDto.setKidId(kid.getKidId());
-                    loginResultDto.setKidName(kid.getKidName());
-                    loginResultDto.setAccept(kid.getAccept());
-                    loginResultDto.setProfileImg(kid.getProfileImg());
+                response.setHeader("jwt-access-token", tokenDto.getAccessToken());
+                response.setHeader("jwt-refresh-token", tokenDto.getRefreshToken());
 
-                    KinderClass kinderclass = kinderRepository.findByClassCode(kid.getClassCode());
-                    loginResultDto.setKinderCode(kinderclass.getKinder().getKinderCode());
-                    loginResultDto.setKinderName(kinderclass.getKinder().getKinderName());
-                    loginResultDto.setClassCode(kinderclass.getClassCode());
-                    loginResultDto.setClassName(kinderclass.getClassName());
+                int result = loginService.setRefreshToken(
+                        new TokenUpdateDto(tokenDto.getRefreshToken(), loginResultDto.getUserId()));
 
-                } else {
-                    loginResultDto.setAccept(kid.getAccept());
-                    return new ResponseEntity<>(loginResultDto, HttpStatus.OK);
+                if (result != 1) {
+                    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
                 }
-
-            }else if(userType == 2){ // 선생님
-
-                Teacher teacher = user.getTeacher();
-                if(teacher.getAccept() == 2) {
-                    loginResultDto.setUserId(user.getUserId());
-                    loginResultDto.setUserName(user.getUserName());
-                    loginResultDto.setType(user.getType());
-                    loginResultDto.setAccept(teacher.getAccept());
-                    loginResultDto.setStateCode(teacher.getStateCode());
-                    loginResultDto.setProfileImg(teacher.getProfileImg());
-
-                    KinderClass kinderclass = kinderRepository.findByClassCode(teacher.getClassCode());
-                    loginResultDto.setKinderCode(kinderclass.getKinder().getKinderCode());
-                    loginResultDto.setKinderName(kinderclass.getKinder().getKinderName());
-                    loginResultDto.setClassCode(kinderclass.getClassCode());
-                    loginResultDto.setClassName(kinderclass.getClassName());
-                } else {
-                    loginResultDto.setAccept(teacher.getAccept());
-                    return new ResponseEntity<>(loginResultDto, HttpStatus.OK);
-                }
-
-            }else if(userType == 9){ // 관리자
-                Teacher teacher = user.getTeacher();
-                KinderClass kinderclass = kinderRepository.findByClassCode(teacher.getClassCode());
-                loginResultDto.setKinderCode(kinderclass.getKinder().getKinderCode());
-                loginResultDto.setKinderName(kinderclass.getKinder().getKinderName());
             }
-
-            TokenDto tokenDto = jwtService.create("userid", loginResultDto.getUserId(), "user");
-
-            response.setHeader("jwt-access-token", tokenDto.getAccessToken());
-            response.setHeader("jwt-refresh-token", tokenDto.getRefreshToken());
-
-            int result = loginService.setRefreshToken(
-                    new TokenUpdateDto(tokenDto.getRefreshToken(), loginResultDto.getUserId()));
-
-            if (result == 1) {
-                return new ResponseEntity<>(loginResultDto, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
+            return new ResponseEntity<>(loginResultDto, HttpStatus.OK);
 
         }
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
--- backend/src/main/java/com/myapp/backend/service/LoginService.java
+++ backend/src/main/java/com/myapp/backend/service/LoginService.java
@@ -1,10 +1,17 @@
 package com.myapp.backend.service;
 
+import com.myapp.backend.domain.kinder.KinderClass;
 import com.myapp.backend.domain.login.LoginDto;
+import com.myapp.backend.domain.login.LoginResultDto;
 import com.myapp.backend.domain.login.TokenUpdateDto;
+import com.myapp.backend.domain.user.Kid;
+import com.myapp.backend.domain.user.Teacher;
 import com.myapp.backend.domain.user.User;
+import com.myapp.backend.repository.KinderRepository;
 import com.myapp.backend.repository.LoginRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -14,6 +21,9 @@ public class LoginService {
 
     @Autowired
     LoginRepository loginRepository;
+
+    @Autowired
+    KinderRepository kinderRepository;
 
     private static final int SUCCESS = 1;
     private static final int FAIL = -1;
@@ -53,6 +63,63 @@ public class LoginService {
         }
         return null;
     }
+
+    @Transactional
+    public LoginResultDto setLoginResult(int type, User user) {
+        LoginResultDto loginResultDto = new LoginResultDto();
+        if(type == 1){ // 학부모
+            Kid kid = user.getKids().get(0);
+
+            if(kid.getAccept() == 2){
+
+                loginResultDto.setUserId(user.getUserId());
+                loginResultDto.setUserName(user.getUserName());
+                loginResultDto.setType(user.getType());
+                loginResultDto.setKidId(kid.getKidId());
+                loginResultDto.setKidName(kid.getKidName());
+                loginResultDto.setAccept(kid.getAccept());
+                loginResultDto.setProfileImg(kid.getProfileImg());
+
+                KinderClass kinderclass = kinderRepository.findByClassCode(kid.getClassCode());
+                loginResultDto.setKinderCode(kinderclass.getKinder().getKinderCode());
+                loginResultDto.setKinderName(kinderclass.getKinder().getKinderName());
+                loginResultDto.setClassCode(kinderclass.getClassCode());
+                loginResultDto.setClassName(kinderclass.getClassName());
+
+            } else {
+                loginResultDto.setAccept(kid.getAccept());
+            }
+
+        }else if(type == 2){ // 선생님
+
+            Teacher teacher = user.getTeacher();
+            if(teacher.getAccept() == 2) {
+                loginResultDto.setUserId(user.getUserId());
+                loginResultDto.setUserName(user.getUserName());
+                loginResultDto.setType(user.getType());
+                loginResultDto.setAccept(teacher.getAccept());
+                loginResultDto.setStateCode(teacher.getStateCode());
+                loginResultDto.setProfileImg(teacher.getProfileImg());
+
+                KinderClass kinderclass = kinderRepository.findByClassCode(teacher.getClassCode());
+                loginResultDto.setKinderCode(kinderclass.getKinder().getKinderCode());
+                loginResultDto.setKinderName(kinderclass.getKinder().getKinderName());
+                loginResultDto.setClassCode(kinderclass.getClassCode());
+                loginResultDto.setClassName(kinderclass.getClassName());
+            } else {
+                loginResultDto.setAccept(teacher.getAccept());
+            }
+
+        }else { // 관리자
+            Teacher teacher = user.getTeacher();
+            KinderClass kinderclass = kinderRepository.findByClassCode(teacher.getClassCode());
+            loginResultDto.setKinderCode(kinderclass.getKinder().getKinderCode());
+            loginResultDto.setKinderName(kinderclass.getKinder().getKinderName());
+        }
+
+        return loginResultDto;
+    }
+
 
 
 }

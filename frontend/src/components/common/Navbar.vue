e6666723ee606b13e3c94134dbf82f119d01e6f3
ryu <munam4@naver.com>
2021-07-27 23:46:34 +0900

    fix Navbar Form

--- frontend/src/components/common/Navbar.vue
+++ frontend/src/components/common/Navbar.vue
@@ -1,75 +1,69 @@
 <template>
     <div style="background-color: rgb(250, 215, 73);">
-      <!-- 유저 정보 가져오는 로직 손보기. -->
-        <ul class="nav">
-              <li class="nav-item">
-                  <!-- 메인으로 -->
-                  <!-- 밥먹고 와서 여기 -->
-                  <button @click="moveToMainPage">로고</button>
-              </li>
-              <li class="nav-item">
-                  <!-- 메인으로 -->
-                  <button @click="moveToMainPage">서비스명</button>
-              </li>
-              <li class="nav-item">
-                  <button 
-                  class="nav-link disabled" 
-                  href="#"
-                  v-if="$store.state.user.type == 2">{{ $store.state.user.className }} {{ $store.state.user.userName }} 선생님</button>
-                  <button 
-                  class="nav-link disabled" 
-                  href="#"
-                  v-if="$store.state.user.type == 1">{{ $store.state.user.className }} {{ $store.state.user.userName }} 보호자님</button>
-              </li>
 
-              <div class="dropdown">
-                <button class="btn btn-link text-decoration-none " href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                  <v-avatar>
-                    <img src="@/assets/seo.jpg" alt="profile-image">
-                  </v-avatar>
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                  <!-- 마이페이지 본인 id 쿼리 넘겨주기 -->
-                  <li><button @click="$router.push({ name: 'MyPage', params: { userId: $store.state.user.userId }})">마이페이지</button></li>
-                  <li><button>알림 끄기</button></li>
-                  <li><button @click="logout">로그아웃</button></li>
-                </ul>
-              </div>
+      <v-app-bar
+        color="mainColor accent-4"
+        dense
+        dark
+      >
+        <v-btn icon plain @click="moveToMainPage">
+          <v-icon>로고</v-icon>
+        </v-btn>
+        <v-btn plain @click="moveToMainPage">
+          <v-icon>Icontact</v-icon>
+        </v-btn>
+        
+        <v-spacer></v-spacer>
+        <p v-if="$store.state.user.type == 2" style="color: black; margin-bottom:0px; margin-right:20px">{{ $store.state.user.className }} {{ $store.state.user.userName }} 선생님</p>
+        <p v-if="$store.state.user.type == 1">{{ $store.state.user.className }} {{ $store.state.user.userName }} 보호자님</p>
+        <v-menu
+          left
+          bottom
+        >
+          <template v-slot:activator="{ on, attrs }" >
+            <v-btn
+              icon
+              v-bind="attrs"
+              v-on="on"
+            style="margin-right:15px">
+              <v-avatar size="40">
+                <img :src="require('@/assets/profileImg/' + $store.state.user.userId + '.jpg')" alt="profile-image">
+              </v-avatar>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item @click="$router.push({ name: 'MyPage', params: { userId: $store.state.user.userId }})">
+              <v-list-item-title >마이페이지</v-list-item-title>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title>알람끄기</v-list-item-title>
+            </v-list-item>
+            <v-list-item @click="logout">
+              <v-list-item-title >로그아웃</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
 
 
-                <div class="dropdown">
-                  <button class="btn btn-link text-decoration-none " href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                    <v-badge 
-                      avatar
-                      bordered
-                      overlap
-                    >
-                      <template v-slot:badge>
-                        <v-avatar>
-                          <v-img src="@/assets/flaticon/bells.png"></v-img>
-                        </v-avatar>
-                      </template>
+        <v-btn icon>  
+          <v-badge
+            overlap
+            dot
+            color="pink"
+          >
+            <v-avatar size="40">
+              <v-img src="https://image.flaticon.com/icons/png/512/2645/2645883.png"></v-img>
+            </v-avatar>
+          </v-badge>
+        </v-btn>
 
-                      <v-avatar size="30">
-                        <v-img src="@/assets/flaticon/bells.png"></v-img>
-                      </v-avatar>
-                    </v-badge>
-                  </button>
-                  <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                    <!-- 마이페이지 본인 id 쿼리 넘겨주기 -->
-                    <li>알람1</li>
-                    <li>알람2</li>
-                    <li>알람3</li>
-                  </ul>
-                </div>
-        </ul>
+      </v-app-bar>
     </div>
 </template>
 
 <script>
 export default {
   name: 'Navbar',
-  
   methods: {
     logout() {
       if (confirm("정말 로그아웃하시겠습니까?")) {
@@ -81,7 +75,7 @@ export default {
       if (this.$route.path !== '/') {
         this.$router.push({ name: 'MainPage' })
       }
-    }
+    },
   }
 }
 </script>
